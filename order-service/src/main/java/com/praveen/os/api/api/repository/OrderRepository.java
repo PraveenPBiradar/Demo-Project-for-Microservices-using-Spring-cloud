fb349d32099597508a6bd8e68318b8b380621aaf
Praveen Biradar <47683912+PraveenPBiradar@users.noreply.github.com>
2021-04-14 00:10:19 +0530

    Delete order-service/src/main/java/com/praveen/os/api directory

--- order-service/src/main/java/com/praveen/os/api/api/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.praveen.os.api.repository;
-
-import com.praveen.os.api.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import javax.persistence.Id;
-
-public interface OrderRepository extends JpaRepository<Order,Integer> {
-}
